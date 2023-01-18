417dbb0777304cab75b7cc865b4aee614ec584ff
Sangeeth Joseph <you@example.com>
2023-01-18 13:52:06 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+import requests
+app = FastAPI()
+
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/summarize/{text}")
+def summarize_report(text: str):
+    response = requests.post("https://api.ai21.com/studio/v1/experimental/summarize",
+    headers={"Authorization": "Bearer CUO7L6sfoRoZPYWoRlmox7bWvkUi26RQ"},
+    json={
+        "text": text
+    }
+    )
+    print("summary",response)
+    return {"summary": response}
