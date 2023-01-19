ca0086cdeb3455a34d035f1aaae788c5b001f26b
Sangeeth Joseph <you@example.com>
2023-01-19 19:04:46 +0530

    added Procfile

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 token = os.getenv("token")
 print("token",token)
